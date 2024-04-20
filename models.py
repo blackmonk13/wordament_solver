732d8fd3212e432008dac3cbcea0dbacd38dcf53
blackmonk13 <spencervalentino13@gmail.com>
2024-04-20 15:03:49 +0300

    create Trie data structure

--- models.py
+++ models.py
@@ -1,5 +1,30 @@
 from typing import List, Tuple, Dict
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.end_of_word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.end_of_word
+
 class Cell:
     def __init__(self, letter: str, score: int):
         self.letter = letter
